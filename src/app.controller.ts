3c2684cd1a9d4b250ae169301e04cdb6a73c4ba6
KausaChangs <changayakausa@gmail.com>
2024-01-01 10:59:30 +0200

    delete endpoint is working

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,6 +6,7 @@ import {
   Delete,
   Param,
   Body,
+  HttpCode,
 } from '@nestjs/common';
 import { report } from 'process';
 import { findIndex } from 'rxjs';
@@ -74,8 +75,15 @@ export class AppController {
     return data.report[reportIndex];
   }
 
+  @HttpCode(204)
   @Delete(':id')
   deleteReport(@Param('id') id: string) {
     const reportIndex = data.report.findIndex((report) => report.id === id);
+
+    if (reportIndex === -1) return;
+
+    data.report.splice(reportIndex, 1);
+
+    return;
   }
 }
