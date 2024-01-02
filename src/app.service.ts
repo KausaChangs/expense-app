ccda764423549a94d4a3816862a683999fad82ce
KausaChangs <changayakausa@gmail.com>
2024-01-02 20:36:59 +0200

    moved all getAllReports business logic from the controller

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,1 +1,7 @@
-class AppService {}
+import { ReportType, data } from 'src/data';
+
+class AppService {
+  getAllReports(type: ReportType) {
+    return data.report.filter((report) => report.type === type);
+  }
+}
